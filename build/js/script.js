0a455e3d596adf2811971dc3c5555a53992fcf4e
Feizerr <anastasiapred@gmail.com>
2021-10-02 00:40:52 +0300

    добавляет меню

--- build/js/script.js
+++ build/js/script.js
@@ -1,15 +1,40 @@
-'use strict';
-var pageHeader = document.querySelector('.page-header');
-var headerToggle = document.querySelector('.page-header__toggle');
+// 'use strict';
+// var pageHeader = document.querySelector('.page-header');
+// var headerToggle = document.querySelector('.page-header__toggle');
 
-pageHeader.classList.remove('page-header--nojs');
+// pageHeader.classList.remove('page-header--nojs');
+
+// headerToggle.addEventListener('click', function () {
+//   if (pageHeader.classList.contains('page-header--closed')) {
+//     pageHeader.classList.remove('page-header--closed');
+//     pageHeader.classList.add('page-header--opened');
+//   } else {
+//     pageHeader.classList.add('page-header--closed');
+//     pageHeader.classList.remove('page-header--opened');
+//   }
+// });
+
+'use strict';
+
+var inputForSearch = document.querySelector('#search')
+
+var removePlaceholderValue = function () {
+  window.addEventListener('resize', function () {
+    if (window.innerWidth <= 767) {
+      inputForSearch.placeholder = "";
+    }
+
+    else {
+      inputForSearch.placeholder = "Type here to search";
+    }
+  })
+}
+
+removePlaceholderValue();
+
+var pageHeader = document.querySelector('.header');
+var headerToggle = document.querySelector('.header__toggle');
 
 headerToggle.addEventListener('click', function () {
-  if (pageHeader.classList.contains('page-header--closed')) {
-    pageHeader.classList.remove('page-header--closed');
-    pageHeader.classList.add('page-header--opened');
-  } else {
-    pageHeader.classList.add('page-header--closed');
-    pageHeader.classList.remove('page-header--opened');
-  }
+  pageHeader.classList.toggle('header--active');
 });
